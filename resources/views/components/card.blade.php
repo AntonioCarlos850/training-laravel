6784e07fee4233b0f06568d8085c2709f1332aaa
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-06-04 22:58:05 -0300

    enhacement/home: reducing card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<article class="col-dt-3 col-mb-5 card p-2">
+<article class="col-dt-3 col-mb-5 card p-2"></article>
     <a class="content row center row direction-column space-evenly" href="{{route('product',$slug)}}">
         <img src={{$image}} alt="nike show">
         <h3>{{$slot}}</h3>

9001a447c0f6fcc151dc4aa6ac308e6a570494cb
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-06-06 00:44:13 -0300

    add firts style to product page

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -1,8 +1,23 @@
 @extends('layouts.app')
 
+@push('name')
+    {{$product->name}}
+@endpush
+
+@push('styles')
+    <link rel="stylesheet" href="{{asset('css/product.css')}}">
+@endpush
+
 @section('content')
-    <h1>{{$product->name}}</h1>
-    <a href="{{route('search',["category"=>$product->category->slug])}}"><h2>{{$product->category->name}}</h2></a>
-    <span>{{$product->value}}</span>
-    <img src="{{$product->image}}" alt="" width="250">
+    <section class="row w-100">
+        <main class="col-dt-6 col-mb-10">
+            <a href="{{route('search',["category"=>$product->category->slug])}}"><h2>{{$product->category->name}}</h2></a>
+            <img src="{{$product->image}}" alt="" width="250">
+        </main>
+        <aside class="col-dt-4 col-mb-10">
+            <span>{{$product->category->name}}</span>
+            <h1>{{$product->name}}</h1>
+            <span>$ {{$product->value}}</span>
+        </aside>
+    </section>
 @endsection

51540e903ff87f070f37e79b834a5fa7cd4cecac
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-06-22 01:22:32 -0300

    defining a gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::defaultView('pagination::default');
+
+        Gate::define('admin',function(){
+            return auth()->user()?->id == 1;
+        });
     }
 }

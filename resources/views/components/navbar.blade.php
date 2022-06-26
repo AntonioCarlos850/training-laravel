05e338e04775e114a4763642e65e0ba04331d67e
Antonio Carlos <antoniocarlosjr850@gmail.com>
2022-06-26 02:11:49 -0300

    adjusting class

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,8 @@
 <nav id="navbar" class="row w-100 bg-white center">
-    <a href="{{route('index')}}" class="col-mb-4">
+    <a href="{{route('index')}}" class="col-dt-2 col-mb-4">
         <img src="{{asset('image/logoipsum-logo-9.svg')}}" alt="logipsum">
     </a>
-    <form action="{{route('search')}}" method="get" class="col-mb-6">
+    <form action="{{route('search')}}" method="get" class="col-dt-4 col-mb-6">
         <input type="text" name="q" id="q" value="{{request('q') ?? ""}}"><button type="submit">
             <i class="fa-solid fa-magnifying-glass"></i>
         </button>
